5bf0a2e9d504ba37687f634aecf4f66d942d748c
businapavel <businapavel@gmail.com>
2026-01-20 20:01:36 +0100

    trip detail page

---
+++ src/pages/trip-detail/trip-detail.tsx
@@ -0,0 +1,16 @@
+import { Trip } from '@/types/trip';
+import { ThemeConfig } from '@/config/theme-config';
+import { ThemeRenderer } from '@/components/trip-themes/theme-renderer';
+
+interface TripDetailProps {
+  trip: Trip;
+  config: ThemeConfig;
+}
+
+export function TripDetail({ trip, config }: TripDetailProps) {
+  return (
+    <div className="min-h-screen">
+      <ThemeRenderer trip={trip} config={config} />
+    </div>
+  );
+}
