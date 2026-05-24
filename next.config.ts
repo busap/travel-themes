284c520491c1c5a6e6315f9f8a22a749710979d5
businapavel <businapavel@gmail.com>
2026-05-24 17:07:06 +0200

    next config images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	images: {
 		formats: ["image/avif", "image/webp"],
+		minimumCacheTTL: 31536000,
+		deviceSizes: [640, 1080, 1920, 2560],
+		imageSizes: [256, 384],
 		remotePatterns: [
 			{
 				protocol: "https",
